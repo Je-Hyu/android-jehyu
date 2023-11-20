46a18f53f3ac12951b1510082637eafbe74ddba0
2wndrhs <2wndrhs@gmail.com>
2023-11-20 17:22:55 +0900

    refactor: 홈 화면 카드 리스트 뷰 recycler view로 리팩토링

--- app/src/main/java/com/example/jehyuhassu/HomeFragment.java
+++ app/src/main/java/com/example/jehyuhassu/HomeFragment.java
@@ -2,15 +2,23 @@ package com.example.jehyuhassu;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 
+import com.example.jehyuhassu.databinding.CardListItemBinding;
 import com.example.jehyuhassu.databinding.FragmentHomeBinding;
+import com.example.jehyuhassu.model.CardListItem;
+
+import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
     private FragmentHomeBinding binding;
@@ -19,7 +27,6 @@ public class HomeFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
     @Override
@@ -39,6 +46,61 @@ public class HomeFragment extends Fragment {
             }
         });
 
+        ArrayList<CardListItem> items = new ArrayList<>();
+        items.add(new CardListItem(R.drawable.dak, "내가찜한닭", "10:00 ~ 21:40", new String[]{"IT대학"}));
+        items.add(new CardListItem(R.drawable.real_fry, "리얼후라이", "16:00 ~ 01:00", new String[]{"IT대학"}));
+        items.add(new CardListItem(R.drawable.chwihyang, "취향", "10:30 ~ 20:30", new String[]{"IT대학"}));
+
+        binding.homeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        binding.homeRecyclerView.setAdapter(new HomeListAdapter(items));
+
         return view;
+    }
+
+    private class HomeCardViewHolder extends RecyclerView.ViewHolder {
+        private CardListItemBinding binding;
+
+        private HomeCardViewHolder(CardListItemBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+    }
+
+    private class HomeListAdapter extends RecyclerView.Adapter<HomeCardViewHolder> {
+        private ArrayList<CardListItem> items;
+
+        public HomeListAdapter(ArrayList<CardListItem> items) {
+            this.items = items;
+        }
+
+        @NonNull
+        @Override
+        public HomeCardViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_list_item, parent, false);
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int position = binding.homeRecyclerView.getChildAdapterPosition(v);
+                    CardListItem item = items.get(position);
+                    Log.d("HomeFragment", "item clicked: " + item.getName());
+                }
+            });
+
+            return new HomeCardViewHolder(CardListItemBinding.bind(view));
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull HomeCardViewHolder holder, int position) {
+            CardListItem item = items.get(position);
+            holder.binding.cardImageView.setImageResource(item.getImage());
+            holder.binding.cardTitleTextView.setText(item.getName());
+            holder.binding.cardTimeTextView.setText(item.getTime());
+            holder.binding.cardCollegeChipView.setText(item.getTags()[0]);
+        }
+
+        @Override
+        public int getItemCount() {
+            return items.size();
+        }
     }
 }
