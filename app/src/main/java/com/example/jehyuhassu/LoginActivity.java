1cd91048f4a4d6c32a0adb9010c48ba8ea58626a
seominbi <zyuqw1193@naver.com>
2023-12-02 16:44:54 +0900

    리얼타임파이어베이스 환경설정 추가 및 회원가입 함수 사용 예시

--- app/src/main/java/com/example/jehyuhassu/LoginActivity.java
+++ app/src/main/java/com/example/jehyuhassu/LoginActivity.java
@@ -14,6 +14,8 @@ import android.widget.Toast;
 import com.example.jehyuhassu.databinding.ActivityLoginBinding;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
@@ -24,6 +26,7 @@ import com.example.jehyuhassu.Member;
 import com.example.jehyuhassu.LoginRequest;
 import com.example.jehyuhassu.LoginResponse;
 import com.example.jehyuhassu.databinding.ActivityLoginBinding;
+import com.example.jehyuhassu.firebase_model.User;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -45,17 +48,24 @@ public class LoginActivity extends AppCompatActivity {
 
     private FirebaseFirestore database;
 
+    //minb: 리얼타임 데이터 베이스 인스턴스
+    private DatabaseReference mDatabase;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        //minb: 리얼타임 데이터 베이스 인스턴스 초기화
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
         retrofitInterface = RetrofitClient.getRetrofit().create(RetrofitInterface.class);
         preferences = getSharedPreferences("UserInfo", MODE_PRIVATE);
         database = FirebaseFirestore.getInstance();
 
         binding.loginBtn.setOnClickListener(view -> {
+            Log.d("seo", "로그인 버튼 클릭");
             studentNum = binding.loginStudentNumEditText.getText().toString();
             password = binding.loginPasswordEditText.getText().toString();
 
@@ -77,42 +87,11 @@ public class LoginActivity extends AppCompatActivity {
                             String department = loginResponse.getParentDept();
                             String major = loginResponse.getDept();
                             String name = loginResponse.getName();
-                            Member member = new Member(studentNum, department, name, major);
 
-                            database.collection("Member")
-                                    .whereEqualTo("studentNum", studentNum)
-                                    .get()
-                                    .addOnCompleteListener(task -> {
-                                        if (task.isSuccessful()) {
-                                            // firestore에 해당 학번의 멤버가 없다면 추가
-                                            if (task.getResult().getDocuments().isEmpty()) {
-                                                database.collection("Member")
-                                                        .add(member)
-                                                        .addOnCompleteListener(task1 -> {
-                                                            SharedPreferences.Editor editor = preferences.edit();
-                                                            editor.putString("studentNum", studentNum);
-                                                            editor.putString("depart", department);
-                                                            editor.putString("name", name);
-                                                            editor.putString("major", major);
-                                                            editor.apply();
-
-                                                            Log.d("LoginActivity", preferences.getString("studentNum", ""));
-                                                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
-                                                        });
-                                            } else { // firestore에 해당 학번의 멤버가 있다면
-                                                SharedPreferences.Editor editor = preferences.edit();
-                                                editor.putString("studentNum", studentNum);
-                                                editor.putString("depart", department);
-                                                editor.putString("name", name);
-                                                editor.putString("major", major);
-                                                editor.apply();
-
-                                                Log.d("LoginActivity", preferences.getString("studentNum", ""));
-                                                startActivity(new Intent(getApplicationContext(), MainActivity.class));
-                                            }
-                                        }
-                                    });
-
+                            //minb: 회원가입 함수 호출
+                            Log.d("seo", "회원가입 함수 호출 전");
+                            saveUserInfo(Integer.parseInt(studentNum), password,name, department, major);
+                            Log.d("seo", "회원가입 함수 호출 후");
                         }
                     }
 
@@ -124,6 +103,29 @@ public class LoginActivity extends AppCompatActivity {
                 });
             }
         });
+    }
+
+    //minb: 회원가입 함수
+    private void saveUserInfo(int studentId, String pw, String name, String college, String department) {
+        // Firebase Realtime Database를 사용하여 사용자 정보 저장
+        User user = new User(studentId, pw, name, college, department);
+        Log.d("seo", "회원가입 함수");
+        mDatabase.child("Users").child(String.valueOf(studentId)).setValue(user)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            // 데이터 저장 성공
+                            Toast.makeText(LoginActivity.this, "회원가입 성공!", Toast.LENGTH_SHORT).show();
+                            Log.d("minb", "데이터 저장 성공!");
+
+                        } else {
+                            // 데이터 저장 실패
+                            Toast.makeText(LoginActivity.this, "회원가입 실패. 다시 시도하세요.", Toast.LENGTH_SHORT).show();
+                            Log.e("minb", "데이터 저장 실패", task.getException());
+                        }
+                    }
+                });
     }
 
     public void showToast(String msg) {
--- app/src/main/java/com/example/jehyuhassu/firebase_model/User.java
+++ app/src/main/java/com/example/jehyuhassu/firebase_model/User.java
@@ -1,2 +1,55 @@
-package com.example.jehyuhassu.firebase_model;public class User {
+package com.example.jehyuhassu.firebase_model;
+
+import com.google.gson.annotations.SerializedName;
+
+public class User {
+    @SerializedName("studentId")
+    private int studentId;
+    @SerializedName("password")
+    private String pw;
+    @SerializedName("name")
+    private String name;
+    @SerializedName("college")
+    private String college;
+    @SerializedName("department")
+    private String department;
+    public User(int studentId, String pw, String name, String college, String department){
+        this.studentId=studentId;
+        this.pw=pw;
+        this.name=name;
+        this.college=college;
+        this.department=department;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+    public void setPassword(String pw) {
+        this.pw = pw;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setCollege(String college) {
+        this.college = college;
+    }
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public int getStudentId() {
+        return this.studentId;
+    }
+    public String getPassword() {
+        return this.pw;
+    }
+    public String getName() {
+        return this.name;
+    }
+    public String getCollege() {
+        return this.college;
+    }
+    public String getDepartment() {
+        return this.department;
+    }
 }
